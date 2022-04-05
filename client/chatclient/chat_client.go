77c3971b5c2a9494d95a6487b5c39a3b03abae98
MetalMari <ku4er.marina@gmail.com>
2022-04-05 17:29:34 +0300

    Split client module into two ones.

---
+++ client/chatclient/chat_client.go
@@ -0,0 +1,82 @@
+package chatclient
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "chat_go/chat_protos"
+
+	"google.golang.org/grpc"
+)
+
+type Client struct {
+	Endpoint string
+
+	conn   io.Closer
+	client pb.ChatClient
+}
+
+// NewClient creates new chat client.
+func NewClient(endpoint string) (*Client, error) {
+	conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{Endpoint: endpoint, conn: conn, client: pb.NewChatClient(conn)}, nil
+}
+
+// Close closes gRPC connection.
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
+
+// GetUsers gets and prints list of users.
+func (c *Client) GetUsers() {
+	ctx := context.Background()
+
+	r, err := c.client.GetUsers(ctx, &pb.GetUsersRequest{})
+	if err != nil {
+		log.Fatalf("could not get users: %v", err)
+	}
+	log.Printf("Users: %s", r.Users)
+}
+
+type Message struct {
+	LoginFrom string
+	LoginTo   string
+	CreatedAt int32
+	Body      string
+}
+
+// SendMessage sends message contained sender's login, recipient's login,
+// creation timestamp, body-content and prints response from server. 
+func (c *Client) SendMessage(m *Message) {
+	mes := &pb.Message{LoginFrom: m.LoginFrom, LoginTo: m.LoginTo, CreatedAt: m.CreatedAt, Body: m.Body}
+	ctx := context.Background()
+	r, err := c.client.SendMessage(ctx, &pb.SendMessageRequest{Message: mes})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Status: %s", r.Status)
+}
+
+// Gets and prints all messages, given in stream by subscription. 
+func (c *Client) Subscribe(login string) {
+	ctx := context.Background()
+	stream, err := c.client.Subscribe(ctx, &pb.SubscribeRequest{Login: login})
+	if err != nil {
+		log.Fatalf("Cannot receive: %v", err)
+	}
+	for {
+		mes, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Cannot receive: %v", err)
+		}
+		log.Printf("Message: %s", mes)
+	}
+}
--- client/main.go
+++ client/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"io"
 	"log"
-	"time"
 
-	pb "chat_go/chat_protos"
-	"google.golang.org/grpc"
+	cl "chat_go/client/chatclient"
 )
 
 var (
@@ -22,48 +18,20 @@ var (
 
 func main() {
 	flag.Parse()
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	client, err := cl.NewClient(*addr)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
-	c := pb.NewChatClient(conn)
+	defer client.Close()
 
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	var p string = *a
-	switch p {
+	switch *a {
 	case "users":
-		r, err := c.GetUsers(ctx, &pb.GetUsersRequest{})
-		if err != nil {
-			log.Fatalf("could not get users: %v", err)
-		}
-		log.Printf("Users: %s", r.GetUsers())
+		client.GetUsers()
 	case "message":
-		m := &pb.Message{LoginFrom: *from, LoginTo: *to, CreatedAt: 1234, Body: *body}
-
-		k, err := c.SendMessage(ctx, &pb.SendMessageRequest{Message: m})
-		if err != nil {
-			log.Fatalf("could not greet: %v", err)
-		}
-		log.Printf("Status: %s", k.Status)
+		m := cl.Message{LoginFrom: *from, LoginTo: *to, CreatedAt: 1234, Body: *body}
+		client.SendMessage(&m)
 	case "subscribe":
-		stream, err := c.Subscribe(ctx, &pb.SubscribeRequest{Login: *login})
-		if err != nil {
-			log.Fatalf("Cannot receive: %v", err)
-		}
-		for {
-			mes, err := stream.Recv()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatalf("Cannot receive: %v", err)
-			}
-			log.Printf("Message: %s", mes)
-		}
+		client.Subscribe(*login)
 	case "":
 		log.Printf("Choose action")
 	}
