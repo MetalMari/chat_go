575d440da2bf331701902742c13216878f611cd9
MetalMari <ku4er.marina@gmail.com>
2022-05-04 11:43:32 +0300

    Add tests to chat_client module.

---
+++ client/chatclient/chat_client_test.go
@@ -0,0 +1,102 @@
+package chatclient
+
+import (
+	"context"
+	"testing"
+
+	pb "chat_go/chat_protos"
+
+	st "chat_go/storage"
+
+	"chat_go/client/chatclient/mocks"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+
+)
+
+func TestUnitNewClient(t *testing.T) {
+	assert := assert.New(t)
+
+	endpoint := "chat.endpoint"
+	_, err := NewClient(endpoint)
+
+	assert.Nil(err)
+}
+
+func TestGetUsers(t *testing.T) {
+	assert := assert.New(t)
+	ctx := context.Background()
+
+	mockClientConn := &grpc.ClientConn{}
+	mockChatClient := mocks.ChatClientMock{}
+
+	mockGetUsersReply := &pb.GetUsersReply{
+		Users: []*pb.User{
+			{Login: "user1", FullName: "u_user1"},
+			{Login: "user2", FullName: "u_user2"},
+		},
+	}
+	mockGetUsersRequest := &pb.GetUsersRequest{}
+	mockChatClient.On("GetUsers", ctx, mockGetUsersRequest).Return(mockGetUsersReply, nil)
+
+	expectedUsers :=  []*pb.User{
+			{Login: "user1", FullName: "u_user1"},
+			{Login: "user2", FullName: "u_user2"},
+		}
+
+	client := &Client{
+		Endpoint: "123",
+		conn:     mockClientConn,
+		client:   mockChatClient,
+	}
+	users, err := client.GetUsers()
+
+	assert.Nil(err)
+	assert.Equal(expectedUsers, users, "Lists of users should be equal.")
+	mockChatClient.AssertExpectations(t)
+}
+
+func TestSendMessage(t *testing.T) {
+	assert := assert.New(t)
+	ctx := context.Background()
+
+	mockClientConn := &grpc.ClientConn{}
+	mockChatClient := mocks.ChatClientMock{}
+
+	m := &st.Message{
+		LoginFrom: "userA",
+		LoginTo: "userB",
+		Body: "Hello!",
+	}
+
+	mockSendMessageRequest := &pb.SendMessageRequest{
+		Message: &pb.Message{
+			LoginFrom: m.LoginFrom,
+			LoginTo: m.LoginTo,
+			Body: m.Body,
+		},
+	}
+
+	mockSendMessageReply := &pb.SendMessageReply{
+		Status: m.LoginTo + " received message from " + m.LoginFrom,
+	}
+
+	mockChatClient.On("SendMessage", ctx, mockSendMessageRequest).Return(mockSendMessageReply, nil)
+
+	expectedStatus :=  "userB received message from userA"
+
+	client := &Client{
+		Endpoint: "123",
+		conn:     mockClientConn,
+		client:   mockChatClient,
+	}
+	status, err := client.SendMessage(m)
+
+	assert.Nil(err)
+	assert.Equal(expectedStatus, status, "Message status should be equal.")
+	mockChatClient.AssertExpectations(t)
+}
+
+
+
--- go.mod
+++ go.mod
@@ -10,16 +10,25 @@ require (
 require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.2 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.2 // indirect
 	go.etcd.io/etcd/client/v3 v3.5.2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/tools v0.1.2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
